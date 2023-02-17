85685396e4e8f6c2f8000ea8508ceb4051b6cd35
User User <stepkadmitrievichreg67@mail.ru>
2023-02-17 07:00:44 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace соне
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Clear();
+        }
+
+        private void Clear()
+        {
+            this.textBox1.Clear();
+            this.textBox2.Clear();
+            this.listBox1.Items.Clear();
+            this.textBox3.Clear();
+            this.textBox4.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int count = int.Parse(this.textBox1.Text);
+            double x1 = double.Parse(this.textBox2.Text);
+            double x2 = double.Parse(this.textBox3.Text);
+            double A = double.Parse(this.textBox4.Text);
+            var items = new object[count];
+            var dx = (x2 - x1) / (count - 1);
+            var rnd = new Random();
+            for (int i = 0; i < items.Length; i++)
+            {
+                double x = x1 + dx * i;
+                if (x < A)
+                {
+                    items[i]=$"При значении x={x} функция имеет значение {A*Math.Cos(Math.Pow(x,2)/2.0)}";
+                }
+                else
+                {
+                    items[i] =$"При значении x={x} функция имеет значение {A/(2*(Math.Pow(Math.E,x/A)+ Math.Pow(Math.E, -x / A)))}";
+                }
+            }
+
+            this.listBox1.Items.AddRange(items);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar) | (e.KeyChar == Convert.ToChar(",")) | e.KeyChar == '\b') return;
+            else
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar) | (e.KeyChar == Convert.ToChar(",")) | e.KeyChar == '\b') return;
+            else
+                e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar) | (e.KeyChar == Convert.ToChar(",")) | e.KeyChar == '\b') return;
+            else
+                e.Handled = true;
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar) | (e.KeyChar == Convert.ToChar(",")) | e.KeyChar == '\b') return;
+            else
+                e.Handled = true;
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace соне
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Clear();
+        }
+
+        private void Clear()
+        {
+            this.textBox1.Clear();
+            this.textBox2.Clear();
+            this.listBox1.Items.Clear();
+            this.listBox2.Items.Clear();
+            this.textBox3.Clear();
+            this.textBox4.Clear();
+            this.textBox5.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            int count = int.Parse(this.textBox1.Text);
+            double x1 = double.Parse(this.textBox2.Text);
+            double x2 = double.Parse(this.textBox3.Text);
+            double h = double.Parse(this.textBox4.Text);
+            var items1 = new object[count];
+            var items2 = new object[count];
+            var dx = double.Parse(this.textBox4.Text);
+            var eps = double.Parse(this.textBox5.Text);
+           
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
